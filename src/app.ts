5dc1165cf89dd06d972653425a76f8dfed73c40f
Nitesh Kumar <kumar945153@gmail.com>
2026-03-24 19:11:46 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,2 @@
+let name:string="Nitesh";
+console.log(name);
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+//Primitive data types -Number,BigInt,String,Boolean
+let name:string="Nitesh";
+console.log(name);
+
+let age:number=30;
+console.log(age);
+
+let isStudent:boolean=true;
+console.log(isStudent);
+
+//Non-primitive data types {} () []-array, objects, functions
+//Always returns an object
+//let arr:number[]=[1,2,"nitesh"];//it is wrong because we have defined the array as number type but we are trying to push a string in it
+
+let arr:number[]=[1,2,3];
+
+
+//tuple
+let array:number[4]=[1,2,3,4];
+
+198507
